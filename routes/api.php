ffd3d7ad06746ab07325841e1817bbbb66b5ad9c
WilAzDev <jhonazcaratework@gmail.com>
2024-06-29 00:11:19 -0500

    arreglamos jwt ahora si funciona el middleware

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+ 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware('api')->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->name('me');
+});
